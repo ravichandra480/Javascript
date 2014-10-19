6ddaaabeacae3a11887b762aafdf2edfc6c3f775
RAVI CHANDRA RAVULAPATI <ravichandra480@gmail.com>
2014-10-19 19:48:20 +0530

    Configs for structure and un structure database using nodejs
    Structure database will be mysql and un structure database will be
    mongodb

--- node-mongo-express-rest-post-get-example/server.js
+++ node-mongo-express-rest-post-get-example/server.js
@@ -5,38 +5,34 @@ var bodyParser = require('body-parser');
 
 // Instance for express app
 var app = express();
-var router = express.Router();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // Connect to mogo server db ip : 127.0.0.1 , Port : 27017
-var db = mongoose.connect('mongodb://127.0.0.1:27017/test');
-app.use('/api', router);
+var db = mongoose.connect('mongodb://127.0.0.1:27017/user');
 var userSchema = mongoose.Schema({
-    name: String
+   id:Number,
+   name: String,
+   address: String,
+   email:String,
+   phone: String
 })
 var User = mongoose.model('user', userSchema);
-router.get('/', function (req, res) {
-	res.send("<a href='/api/users'>Show Users</a>");
+
+app.get('/', function (req, res) {
+	res.send("<a href='/users'>Show Users</a>");
 })
-router.get('/users', function (req, res) {
+
+app.get('/users', function (req, res) {
 		User.find({},function (err, user) {
 			  if (err) return console.error(err);
 			  console.log(user)
 			  res.send(user);
 		})
 });
-router.get('/users/:name', function (req, res) {
-	user = new User({name : req.params.name});
-	user.save();
-	User.find({},function (err, user) {
-		  if (err) return console.error(err);
-		  console.log(user)
-		  res.json(user);
-	})
-	
-});
-router.post('/users', function (req, res) {
-	user = new User({name : req.body.name});
+app.get('/users/:name', function (req, res) {
+	user = new User({id : req.params.name});
 	user.save();
 	User.find({},function (err, user) {
 		  if (err) return console.error(err);
@@ -44,4 +40,5 @@ router.post('/users', function (req, res) {
 		  res.json(user);
 	})	
 });
+
 app.listen(80);
---
+++ node-mysql-mongodb/README.md
@@ -0,0 +1,4 @@
+Nodejs - Mysqldb -  MongoDb
+==========
+
+Configuration for connecting structure(Mysql) and unstrudture(Mongo) database
